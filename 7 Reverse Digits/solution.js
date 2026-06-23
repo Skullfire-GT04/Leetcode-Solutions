61e391f2443109ba5fac4362d7dea375f7381c97
Skullfire-GT04 <betterthanlastonel0l@gmail.com>
2026-06-24 03:17:15 +0530

    Did a question

---
+++ 7 Reverse Digits/solution.js
@@ -0,0 +1,19 @@
+const reverse = (x) => {
+    let divisor = 1;
+    let digits = [];
+    let isNegative = x < 0;
+    x = Math.abs(x);
+    while(Math.trunc(x / divisor) > 0){
+        digits.push(Math.trunc(x / divisor) % 10);
+        divisor *= 10;
+    }
+    
+    let reversedNum = Number(digits.join(""));
+    if(isNegative) reversedNum = -reversedNum;
+    if(reversedNum > 2 ** 31 - 1) reversedNum = 0;
+    if(reversedNum < -(2 ** 31)) reversedNum = 0;
+    return reversedNum;
+}
+
+console.log(reverse(12000000));
+
